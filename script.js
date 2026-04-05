4c75f215fe9a4e91a3248b67600e4552f1d28eb4
Ayesha Tariq <tariqzeshan65@gmail.com>
2026-04-05 21:57:38 +0500

    Added Resume Builder files

---
+++ script.js
@@ -0,0 +1,10 @@
+function generateResume() {
+    document.getElementById("r-name").innerText =
+        document.getElementById("name").value;
+
+    document.getElementById("r-email").innerText =
+        document.getElementById("email").value;
+
+    document.getElementById("r-skills").innerText =
+        document.getElementById("skills").value;
+}
